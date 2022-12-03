6f3fc543f15e3d269178b5328725ae38b04e5c28
dong2810 <dongvip99@gmail.com>
2022-12-03 16:00:04 +0700

    Initial Commit

---
+++ MindXTest/MindXTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindXTestApp.swift
+//  MindXTest
+//
+//  Created by đông on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MindXTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
